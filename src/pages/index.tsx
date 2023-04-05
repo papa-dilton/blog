611f0b8c0d7e01498f178b7d9bbc89030c4f70cb
Dalton Harrold <76129078+papa-dilton@users.noreply.github.com>
2023-04-05 10:43:08 -0500

    Add Blog types and testing module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <h1 id='head'>Hello, World!</h1>
+      <h1 className={styles.head}>Hello, World!</h1>
+      <a href="allposts"><button>All Posts</button></a>
     </>
   )
 }
